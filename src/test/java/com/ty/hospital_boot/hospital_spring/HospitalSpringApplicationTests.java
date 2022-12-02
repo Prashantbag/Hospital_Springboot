3bd7736573fec54d798b6cd83120a415eb52c262
Prashantbag <prashant.bag14@gmail.com>
2022-12-02 12:32:44 +0530

    initial COmmit

---
+++ src/test/java/com/ty/hospital_boot/hospital_spring/HospitalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.hospital_boot.hospital_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
