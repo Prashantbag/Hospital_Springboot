781edbf36d6dc6db1478f67408637b6101920cf7
Prashantbag <prashant.bag14@gmail.com>
2022-12-02 15:25:37 +0530

    created DTO for the application

--- src/main/java/com/ty/hospital_boot/hospital_spring/dto/Encounter.java
+++ src/main/java/com/ty/hospital_boot/hospital_spring/dto/Encounter.java
@@ -1,5 +1,87 @@
 package com.ty.hospital_boot.hospital_spring.dto;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Encounter {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String causeofadmit;
+	private String admitdate;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Branch> branch;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Medorder> medorder ;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getCauseofadmit() {
+		return causeofadmit;
+	}
+
+
+	public void setCauseofadmit(String causeofadmit) {
+		this.causeofadmit = causeofadmit;
+	}
+
+
+	public String getAdmitdate() {
+		return admitdate;
+	}
+
+
+	public void setAdmitdate(String admitdate) {
+		this.admitdate = admitdate;
+	}
+
+
+	public List<Branch> getBranch() {
+		return branch;
+	}
+
+
+	public void setBranch(List<Branch> branch) {
+		this.branch = branch;
+	}
+
+
+	public List<Medorder> getMedorder() {
+		return medorder;
+	}
+
+
+	public void setMedorder(List<Medorder> medorder) {
+		this.medorder = medorder;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Encounter [id=" + id + ", causeofadmit=" + causeofadmit + ", admitdate=" + admitdate + ", branch="
+				+ branch + ", medorder=" + medorder + "]";
+	}
+	
+	
+	
 }
